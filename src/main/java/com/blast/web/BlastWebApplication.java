efcfc5e229b225ef8ba350b456c60efbba4c8ac6
haroon-ahmedy92 <haroonamani92@gmail.com>
2026-03-04 21:27:34 +0300

    Replace Laravel app with Spring Boot + Thymeleaf version (Laravel moved to legacy-laravel)

---
+++ src/main/java/com/blast/web/BlastWebApplication.java
@@ -0,0 +1,12 @@
+package com.blast.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlastWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlastWebApplication.class, args);
+    }
+}
